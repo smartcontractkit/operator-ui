better-blockchain/chainlink

Create useErrorHandler and useLoadingPlaceholder hooks

operator_ui/src/pages/Jobs/Definition.tsx
@@ -17,6 +17,8 @@ import Content from 'components/Content'
 import PrettyJson from 'components/PrettyJson'
 import { JobSpec } from 'core/store/models'
 import jobSpecDefinition from 'utils/jobSpecDefinition'
+import { useErrorHandler } from 'customHooks/useErrorHandler'
+import { useLoadingPlaceholder } from 'customHooks/useLoadingPlaceholder'
 
 const definitionStyles = (theme: Theme) =>
   createStyles({
@@ -38,8 +40,9 @@ const Definition: React.FC<
 > = ({ classes, match }) => {
   const { jobSpecId } = match.params
 
-  const [error, setError] = React.useState()
   const [jobSpec, setJobSpec] = React.useState<ApiResponse<JobSpec>['data']>()
+  const { error, ErrorComponent, setError } = useErrorHandler()
+  const { LoadingPlaceholder } = useLoadingPlaceholder(!error && !jobSpec)
 
   React.useEffect(() => {
     document.title = 'Job Definition'
@@ -50,13 +53,13 @@ const Definition: React.FC<
       .getJobSpec(jobSpecId)
       .then((response) => setJobSpec(response.data))
       .catch(setError)
-  }, [jobSpecId])
+  }, [jobSpecId, setError])
 
   return (
     <Content>
       <Card>
-        {error && <div>Error while fetching data: {JSON.stringify(error)}</div>}
-        {!error && !jobSpec && <div>Fetching...</div>}
+        <ErrorComponent />
+        <LoadingPlaceholder />
         {!error && jobSpec && (
           <CardContent>
             <Grid container spacing={0}>

operator_ui/src/pages/Jobs/Errors.tsx
@@ -15,14 +15,17 @@ import Button from 'components/Button'
 import Content from 'components/Content'
 import { JobSpec } from 'core/store/models'
 import { localizedTimestamp, TimeAgo } from '@chainlink/styleguide'
+import { useErrorHandler } from 'customHooks/useErrorHandler'
+import { useLoadingPlaceholder } from 'customHooks/useLoadingPlaceholder'
 
 export const JobsErrors: React.FC<RouteComponentProps<{
   jobSpecId: string
 }>> = ({ match }) => {
   const { jobSpecId } = match.params
 
-  const [error, setError] = React.useState()
   const [jobSpec, setJobSpec] = React.useState<ApiResponse<JobSpec>['data']>()
+  const { error, ErrorComponent, setError } = useErrorHandler()
+  const { LoadingPlaceholder } = useLoadingPlaceholder(!error && !jobSpec)
 
   const fetchJobSpec = React.useCallback(
     async () =>
@@ -30,7 +33,7 @@ export const JobsErrors: React.FC<RouteComponentProps<{
         .getJobSpec(jobSpecId)
         .then((response) => setJobSpec(response.data))
         .catch(setError),
-    [jobSpecId],
+    [jobSpecId, setError],
   )
 
   React.useEffect(() => {
@@ -57,8 +60,8 @@ export const JobsErrors: React.FC<RouteComponentProps<{
 
   return (
     <Content>
-      {error && <div>Error while fetching data: {JSON.stringify(error)}</div>}
-      {!error && !jobSpec && <div>Fetching...</div>}
+      <ErrorComponent />
+      <LoadingPlaceholder />
       {!error && jobSpec && (
         <Card>
           <Table>

operator_ui/src/pages/Jobs/Show.tsx
@@ -22,6 +22,8 @@ import { JobsErrors } from './Errors'
 import { RegionalNav } from './RegionalNav'
 import { ApiResponse, PaginatedApiResponse } from '@chainlink/json-api-client'
 import { JobSpec, JobRun } from 'core/store/models'
+import { useErrorHandler } from 'customHooks/useErrorHandler'
+import { useLoadingPlaceholder } from 'customHooks/useLoadingPlaceholder'
 
 export type JobData = {
   jobSpec?: ApiResponse<JobSpec>['data']
@@ -86,12 +88,13 @@ const DEFAULT_PAGE = 1
 const RECENT_RUNS_COUNT = 5
 
 export const JobsShow: React.FC<Props> = ({ match, showJobRunsCount = 5 }) => {
-  const [error, setError] = React.useState()
   const [state, setState] = React.useState<JobData>({
     recentRuns: [],
     recentRunsCount: 0,
   })
   const { jobSpec, recentRuns, recentRunsCount } = state
+  const { error, ErrorComponent, setError } = useErrorHandler()
+  const { LoadingPlaceholder } = useLoadingPlaceholder(!error && !jobSpec)
 
   const { jobSpecId } = match.params
 
@@ -112,7 +115,7 @@ export const JobsShow: React.FC<Props> = ({ match, showJobRunsCount = 5 }) => {
         })
       })
       .catch(setError)
-  }, [jobSpecId])
+  }, [jobSpecId, setError])
 
   return (
     <div>
@@ -124,10 +127,8 @@ export const JobsShow: React.FC<Props> = ({ match, showJobRunsCount = 5 }) => {
           path={`${match.path}`}
           render={() => (
             <Content>
-              {error && (
-                <div>Error while fetching data: {JSON.stringify(error)}</div>
-              )}
-              {!error && !jobSpec && <div>Fetching...</div>}
+              <ErrorComponent />
+              <LoadingPlaceholder />
               {!error && jobSpec && (
                 <Grid container spacing={24}>
                   <Grid item xs={8}>

smartcontractkit/operator-ui

src/pages/Jobs/Definition.tsx
@@ -17,6 +17,8 @@ import Content from 'components/Content'
 import PrettyJson from 'components/PrettyJson'
 import { JobSpec } from 'core/store/models'
 import jobSpecDefinition from 'utils/jobSpecDefinition'
+import { useErrorHandler } from 'hooks/useErrorHandler'
+import { useLoadingPlaceholder } from 'hooks/useLoadingPlaceholder'
 
 const definitionStyles = (theme: Theme) =>
   createStyles({
@@ -38,8 +40,9 @@ const Definition: React.FC<
 > = ({ classes, match }) => {
   const { jobSpecId } = match.params
 
-  const [error, setError] = React.useState()
   const [jobSpec, setJobSpec] = React.useState<ApiResponse<JobSpec>['data']>()
+  const { error, ErrorComponent, setError } = useErrorHandler()
+  const { LoadingPlaceholder } = useLoadingPlaceholder(!error && !jobSpec)
 
   React.useEffect(() => {
     document.title = 'Job Definition'
@@ -50,13 +53,13 @@ const Definition: React.FC<
       .getJobSpec(jobSpecId)
       .then((response) => setJobSpec(response.data))
       .catch(setError)
-  }, [jobSpecId])
+  }, [jobSpecId, setError])
 
   return (
     <Content>
       <Card>
-        {error && <div>Error while fetching data: {JSON.stringify(error)}</div>}
-        {!error && !jobSpec && <div>Fetching...</div>}
+        <ErrorComponent />
+        <LoadingPlaceholder />
         {!error && jobSpec && (
           <CardContent>
             <Grid container spacing={0}>

src/pages/Jobs/Errors.tsx
@@ -15,14 +15,17 @@ import Button from 'components/Button'
 import Content from 'components/Content'
 import { JobSpec } from 'core/store/models'
 import { localizedTimestamp, TimeAgo } from '@chainlink/styleguide'
+import { useErrorHandler } from 'hooks/useErrorHandler'
+import { useLoadingPlaceholder } from 'hooks/useLoadingPlaceholder'
 
 export const JobsErrors: React.FC<RouteComponentProps<{
   jobSpecId: string
 }>> = ({ match }) => {
   const { jobSpecId } = match.params
 
-  const [error, setError] = React.useState()
   const [jobSpec, setJobSpec] = React.useState<ApiResponse<JobSpec>['data']>()
+  const { error, ErrorComponent, setError } = useErrorHandler()
+  const { LoadingPlaceholder } = useLoadingPlaceholder(!error && !jobSpec)
 
   const fetchJobSpec = React.useCallback(
     async () =>
@@ -30,7 +33,7 @@ export const JobsErrors: React.FC<RouteComponentProps<{
         .getJobSpec(jobSpecId)
         .then((response) => setJobSpec(response.data))
         .catch(setError),
-    [jobSpecId],
+    [jobSpecId, setError],
   )
 
   React.useEffect(() => {
@@ -57,8 +60,8 @@ export const JobsErrors: React.FC<RouteComponentProps<{
 
   return (
     <Content>
-      {error && <div>Error while fetching data: {JSON.stringify(error)}</div>}
-      {!error && !jobSpec && <div>Fetching...</div>}
+      <ErrorComponent />
+      <LoadingPlaceholder />
       {!error && jobSpec && (
         <Card>
           <Table>

src/pages/Jobs/Show.tsx
@@ -22,6 +22,8 @@ import { JobsErrors } from './Errors'
 import { RegionalNav } from './RegionalNav'
 import { ApiResponse, PaginatedApiResponse } from '@chainlink/json-api-client'
 import { JobSpec, JobRun } from 'core/store/models'
+import { useErrorHandler } from 'hooks/useErrorHandler'
+import { useLoadingPlaceholder } from 'hooks/useLoadingPlaceholder'
 
 export type JobData = {
   jobSpec?: ApiResponse<JobSpec>['data']
@@ -86,12 +88,13 @@ const DEFAULT_PAGE = 1
 const RECENT_RUNS_COUNT = 5
 
 export const JobsShow: React.FC<Props> = ({ match, showJobRunsCount = 5 }) => {
-  const [error, setError] = React.useState()
   const [state, setState] = React.useState<JobData>({
     recentRuns: [],
     recentRunsCount: 0,
   })
   const { jobSpec, recentRuns, recentRunsCount } = state
+  const { error, ErrorComponent, setError } = useErrorHandler()
+  const { LoadingPlaceholder } = useLoadingPlaceholder(!error && !jobSpec)
 
   const { jobSpecId } = match.params
 
@@ -112,7 +115,7 @@ export const JobsShow: React.FC<Props> = ({ match, showJobRunsCount = 5 }) => {
         })
       })
       .catch(setError)
-  }, [jobSpecId])
+  }, [jobSpecId, setError])
 
   return (
     <div>
@@ -124,10 +127,8 @@ export const JobsShow: React.FC<Props> = ({ match, showJobRunsCount = 5 }) => {
           path={`${match.path}`}
           render={() => (
             <Content>
-              {error && (
-                <div>Error while fetching data: {JSON.stringify(error)}</div>
-              )}
-              {!error && !jobSpec && <div>Fetching...</div>}
+              <ErrorComponent />
+              <LoadingPlaceholder />
               {!error && jobSpec && (
                 <Grid container spacing={24}>
                   <Grid item xs={8}>
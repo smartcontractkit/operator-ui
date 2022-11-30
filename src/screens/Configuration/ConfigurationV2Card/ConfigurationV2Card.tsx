2320932d2e11dda1382f23c41ee2709e109442d5
Jordan Krage <jmank88@gmail.com>
2022-11-30 09:23:05 -0600

    convert TOMLCards to ExpansionPanels

--- src/screens/Configuration/ConfigurationV2Card/ConfigurationV2Card.tsx
+++ src/screens/Configuration/ConfigurationV2Card/ConfigurationV2Card.tsx
@@ -7,9 +7,13 @@ import CardHeader from '@material-ui/core/CardHeader'
 import TableCell from '@material-ui/core/TableCell'
 import TableRow from '@material-ui/core/TableRow'
 import Grid from '@material-ui/core/Grid'
+import ExpansionPanel from '@material-ui/core/ExpansionPanel'
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary'
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails'
 
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
 import { prism } from 'react-syntax-highlighter/dist/esm/styles/prism'
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
 
 export const CONFIG_V2_QUERY = gql`
   query FetchConfigV2 {
@@ -31,13 +35,16 @@ export const ConfigurationV2Card = () => {
     return (
       <>
         <Grid item xs={12}>
-          <TOMLCard
-            title="TOML Configuration (v2 config dump)"
-            error={error?.message}
-            loading={loading}
-            toml={data?.configv2.user}
-            showHead
-          />
+          <Card>
+            <CardHeader title="TOML Configuration" />
+            <TOMLPanel
+              title="v2 config dump"
+              error={error?.message}
+              loading={loading}
+              toml={data?.configv2.user}
+              showHead
+            />
+          </Card>
         </Grid>
       </>
     )
@@ -46,22 +53,24 @@ export const ConfigurationV2Card = () => {
   return (
     <>
       <Grid item xs={12}>
-        <TOMLCard
-          title="TOML Configuration (user-specified)"
-          error={error?.message}
-          loading={loading}
-          toml={data?.configv2.user}
-          showHead
-        />
-      </Grid>
-      <Grid item xs={12}>
-        <TOMLCard
-          title="TOML Configuration (effective)"
-          error={error?.message}
-          loading={loading}
-          toml={data?.configv2.effective}
-          showHead
-        />
+        <Card>
+          <CardHeader title="TOML Configuration" />
+          <TOMLPanel
+            title="User specified:"
+            error={error?.message}
+            loading={loading}
+            toml={data?.configv2.user}
+            showHead
+            expanded={true}
+          />
+          <TOMLPanel
+            title="Effective (with defaults):"
+            error={error?.message}
+            loading={loading}
+            toml={data?.configv2.effective}
+            showHead
+          />
+        </Card>
       </Grid>
     </>
   )
@@ -73,6 +82,7 @@ interface Props {
   showHead?: boolean
   title?: string
   error?: string
+  expanded?: boolean
 }
 
 const SpanRow: React.FC = ({ children }) => (
@@ -87,7 +97,7 @@ const FetchingRow = () => <SpanRow>...</SpanRow>
 
 const ErrorRow: React.FC = ({ children }) => <SpanRow>{children}</SpanRow>
 
-const TOMLCard = ({ loading, toml, error = '', title }: Props) => {
+const TOMLPanel = ({ loading, toml, error = '', title, expanded }: Props) => {
   if (error) {
     return <ErrorRow>{error}</ErrorRow>
   }
@@ -96,13 +106,22 @@ const TOMLCard = ({ loading, toml, error = '', title }: Props) => {
     return <FetchingRow />
   }
 
-    //TODO expandable, start collapsed should be possible
+  if (!title) {
+    title = 'TOML'
+  }
+
+  const styles = { display: 'block' }
+
   return (
-    <Card>
-      {title && <CardHeader title={title} />}
-      <SyntaxHighlighter language="toml" style={prism}>
-        {toml}
-      </SyntaxHighlighter>
-    </Card>
+    <ExpansionPanel defaultExpanded={expanded}>
+      <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+        {title}
+      </ExpansionPanelSummary>
+      <ExpansionPanelDetails style={styles}>
+        <SyntaxHighlighter language="toml" style={prism}>
+          {toml}
+        </SyntaxHighlighter>
+      </ExpansionPanelDetails>
+    </ExpansionPanel>
   )
 }
